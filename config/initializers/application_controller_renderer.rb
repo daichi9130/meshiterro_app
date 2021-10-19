5391caeb3bd29f751a0bb9260148f106ab4fa727
daichi9130 <koumedai03@gmail.com>
2021-10-19 06:54:33 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e2392d4664a64c365887a9525161c33b15b5c15d45e5775bc1ce39fcf5be9c53f91057eef7d48c55c621bc21bd36a146ced0d0548114050f446c93ebf6a2b96e'

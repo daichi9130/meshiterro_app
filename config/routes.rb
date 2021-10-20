e77de50623d8aedc78d8a4c381e64b57b8fdc7d3
daichi9130 <koumedai03@gmail.com>
2021-10-20 05:53:31 +0000

    [finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ root to:'homes#top'
 resources :post_images, only: [:new,:create,:index,:show,:destroy] do
 resource :favorites,only: [:create,:destroy]
 resources :post_comments, only: [:create, :destroy]
- end 
+ end
  resources :users, only: [:show,:edit,:update]
 end
